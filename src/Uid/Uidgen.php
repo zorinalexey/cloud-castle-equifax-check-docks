c43ab7413e8aa908564b760043966c2f2ecd6a56
Зорин Алексей <zorinalexey59292@gmail.com>
2023-02-28 22:28:58 +0400

    final

--- src/Uid/Uidgen.php
+++ src/Uid/Uidgen.php
@@ -12,7 +12,7 @@ final class Uidgen
 
     public function __construct()
     {
-        $this->uid = preg_replace('~(\w{8})(\w{4})(\w{3})(\w{4})(\w{12})~u', '$1-$2-1$3-$4-$5', md5(uniqid('', true)));
+        $this->uid = preg_replace('~(\w{8})(\w{4})(\w{3})(\w{3})(\w{12})~u', '$1-$2-1$3-'.$this->getSimbol().'$4-$5', md5(uniqid('', true)));
     }
 
     public function getUid():string
@@ -24,5 +24,11 @@ final class Uidgen
     {
         $hash = new UidHashGenerate($this->uid);
         return $this->uid.'-'. $hash->getHash();
+    }
+
+    private function getSimbol()
+    {
+        $data = [8,9, 'a','b'];
+        return $data[array_rand($data, 1)];
     }
 }
